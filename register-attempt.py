4cf4bf037f0007f534280a2b5d418e15e3a2cd65
aaspradlin <aaspradlin@users.noreply.github.com>
2015-06-04 18:50:11 +0000

    okay

--- register-attempt.py
+++ register-attempt.py
@@ -10,5 +10,18 @@ class MyComponent(ApplicationSession):
 
         def addNumbers(x, y):
            return x + y
-           self.register(addNumbers, 'aero.near.addNumbers');
+        
+        self.register(addNumbers, 'aero.near.addNumbers')
+        print("Session Joined.")
 
+if __name__ == '__main__':
+    runner = ApplicationRunner(
+        environ.get("AUTOBAHN_DEMO_ROUTER", "ws://104.197.76.36:8080/ws"),
+        u"realm1",
+        extra=dict(
+            max_events=5,  # [A] pass in additional configuration
+        ),
+        debug_wamp=False,  # optional; log many WAMP details
+        debug=False,  # optional; log even more details
+    )
+    runner.run(MyComponent)
