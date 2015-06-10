6b00cee339328a69f659071bd1322b15930f7933
ThomasBassa <ThomasBassa@users.noreply.github.com>
2015-06-10 16:00:00 -0400

    Add extra notes to pi servo controller

--- ServoCode/pi-servo-control.py
+++ ServoCode/pi-servo-control.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
-from twisted.internet.defer import inlineCallbacks
+#from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from Adafruit_PWM_Servo_Driver import PWM
-import time
+#import time
+
+# This is executed before anything else
 
 # Initialise the PWM device using the default address
 pwm = PWM(0x40,debug=True)
 
 servoMin = 150  # Min pulse length out of 4096
 servoMax = 600  # Max pulse length out of 4096
-
-
 
 class MyComponent(ApplicationSession):
 
@@ -32,10 +32,11 @@ class MyComponent(ApplicationSession):
         print("Session Joined.")
 
 if __name__ == '__main__':
+    #This is run "first" (really after the servo min/max)
     runner = ApplicationRunner(url = u"ws://104.197.76.36:8080/ws", realm = u"realm1")
     runner.run(MyComponent)
 
-
+# This runs AFTER the runner is killed-- basically never
 pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
 
 
