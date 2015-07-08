064efc8500117f357b1494e84e40a92c8096c034
wesfletch <wesfletchgmail.com>
2015-07-08 14:00:39 -0400

    cleaning up gps code

--- pi-code/gps-test.py
+++ pi-code/gps-test.py
@@ -9,30 +9,6 @@ import time
 
 ser=None
 
-def gpsUpdate(self, gps_string):
-	global ser
-
-
-	gps_string = gps_string[1:] #getting rid of the stupid $ marker at the beginning of the strings
-
-	'''with open('fake_gps.txt', 'r') as data_file:  
-	streamer = NMEAStreamReader(data_file)
-	gps_data = streamer.next()'''
-
-
-	if gps_string[0:5] == "GPRMC":
-		gps_data = {'latitude': 0,'longitude': 0,'lat-heading': 0,'long-heading': 0,'speed': 0}    
-		gps_data['latitude'] = float(gps_string[19:21]) + (float(gps_string[22:27])/60.0)
-		gps_data['longitude'] = float(gps_string[31:34]) + (float(gps_string[34:40])/60.0)
-		gps_data['speed'] = float(gps_string[44:48])
-		latitude = float(gps_string[19:21]) + (float(gps_string[22:27])/60.0)
-		print gps_data
-
-		#print gps_string[0:6:]
-			
-	self.publish(u'aero.near.carPos', gps_data['latitude'], gps_data['longitude'])
-	#sleep(.0333)
-
 def init_serial():
 	global ser  
 	ser = serial.Serial('/dev/ttyAMA0',57600,timeout=1)
@@ -42,6 +18,8 @@ def init_serial():
 #                print("Error: {}".format(e))
 	if ser.isOpen():
 		print 'Open: '
+	data = ser.readline()
+	return data
 
 
 init_serial()
