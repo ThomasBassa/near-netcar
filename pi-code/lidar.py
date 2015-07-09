8bfc2e4cfb68674bae5db3944386b5d25d2f7f41
Ethan Santana <ethansantana01@gmail.com>
2015-07-09 12:39:00 -0400

    created a class for it

--- pi-code/lidar.py
+++ pi-code/lidar.py
@@ -5,27 +5,26 @@
 
 from Adafruit_I2C import write8, readList
 
-LIDARLite_ADDRESS =  0x62          # Default I2C Address of LIDAR-Lite.
-RegisterMeasure   =  0x00          # Register to write to initiate ranging.
-MeasureValue      =  0x04          # Value to initiate ranging.
-RegisterHighLowB  =  0x8f          # Register to get both High and Low bytes in 1 call.
+class Lidar:
+  
+    #
+    def __init__(self, address=0x62):
+    	self.LIDARLite_ADDRESS =  address          # Default I2C Address of LIDAR-Lite.
+      self.RegisterMeasure   =  0x00          # Register to write to initiate ranging.
+      self.MeasureValue      =  0x04          # Value to initiate ranging.
+      self.RegisterHighLowB  =  0x8f          # Register to get both High and Low bytes in 1 call.
+      self.i2c = Adafruit_I2C(self.LIDARLite_ADDRESS, 1)
 
-lidar = Adafruit_I2C(LIDARLite_ADDRESS)
+    #
+    def read(self):
+	    #Write 0x04 to register 0x00
+      self.i2c.write8(self.RegisterMeasure, self.MeasureValue) #Write 0x04 to 0x00
 
-def pull_lidar():
-  #Write 0x04 to register 0x00
-  lidar.write8(RegisterMeasure, MeasureValue) #Write 0x04 to 0x00
+      #delay(1)
 
-  #distanceArray=[0,0] # array to store distance bytes from read function
+      # Read 2 Bytes from LIDAR-Lite Address and store in array
+      distanceArray = self.i2c.readList(self.RegisterHighLowB, 2) 
+      distance = (distanceArray[0] << 8) | distanceArray[1]  # Shift high byte [0] 8 to the left and add low byte [1] to create 16-bit int
 
-  # Read 2byte distance from register 0x8f
-  distance = lidar.readList(RegisterHighLowB, 2) # Read 2 Bytes from LIDAR-Lite Address and store in array
-
-  #int distance = (distanceArray[0] << 8) + distanceArray[1];  # Shift high byte [0] 8 to the left and add low byte [1] to create 16-bit int
-
-  # Print Distance
-  return distance
-
-if __name__ == '__main__':
-    while True:
-        print pull_lidar()
+      #this distance is in cm
+      return distance
