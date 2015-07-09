5d92d90ec4c8a0286d1885df1106f121240bdabf
Ethan Santana <ethansantana01@gmail.com>
2015-07-09 12:43:49 -0400

    created a class for it

--- pi-code/gps-read-serial.py
+++ pi-code/gps-read-serial.py
@@ -7,38 +7,19 @@ import serial
 import time
 
 
-ser=None
-
-def init_serial():
-    global ser  
-    ser = serial.Serial('/dev/ttyAMA0',57600,timeout=1)
-#   try:
-#       ser.open()
-#   except Exception as e:
-#       print("Error: {}".format(e))
-    if ser.isOpen():
-        print 'Open: '
-    data = ser.readline()
-    return data
+class GPS:
+    #
+    def __init__(self):
+	    self.ser = serial.Serial('/dev/ttyAMA0', 57600, timeout=1)
+	
+	    try:
+		    self.ser.open()
+	    except Exception as e:
+            print("Error: {}".format(e))
+	
+	    return self.ser.isOpen()
 
 
-init_serial()
-
-def doALoop():
-    global ser
-    data = ser.readline()
-    print data
-
-'''class MyComponent(ApplicationSession):
-
-
-    def onJoin(self, details):
-        gpsUpdate(self, '$GPRMC,194530.000,A,3051.8007,N,10035.9989,W,1.49,111.67,310714,,,A*74')'''
-        
-    
-if __name__ == '__main__':
-    a = 0
-    while a < 100:
-        whatevs = ser.readline()
-        print whatevs
-        a = a + 1
+	#Attempt to read from the GPS module
+	def gpsRead(self):
+		return self.ser.readline()
