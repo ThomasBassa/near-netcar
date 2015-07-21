f185bf576ef7d553741a0a0520818fa8f8dc8ca9
wesfletch <wesfletchgmail.com>
2015-07-21 14:32:21 -0400

    changing hertz

--- pi-code/pi-master.py
+++ pi-code/pi-master.py
@@ -120,7 +120,7 @@ class MyComponent(ApplicationSession):
 		self.servoMin = 315  # Min pulse length out of 4096
 		self.servoMax = 520  # Max pulse length out of 4096
 		self.servoMiddle = 417 # middle servo value
-		self.pwm.setPWMFreq(60) # Set frequency to 60 Hz
+		self.pwm.setPWMFreq(50) # Set frequency to 60 Hz
 		self.servoChannel = 3        
 
 		self.pwm.setPWM(self.servoChannel, 0, self.servoMiddle) #have vehicle wheels turn to center
