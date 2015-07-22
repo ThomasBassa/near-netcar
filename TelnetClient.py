53e362f65336df41eaa5c280540d3cf1566c4d09
king-of-kong <supergrazc@gmail.com>
2015-07-22 14:10:35 -0400

    Slurp

---
+++ TelnetClient.py
@@ -0,0 +1,9 @@
+import socket
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 4444                # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+s.close
