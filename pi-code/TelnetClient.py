d06bea6fa4c0d16d180fe799f772c16c5d575323
king-of-kong <supergrazc@gmail.com>
2015-07-27 10:08:54 -0400

    Deleting Telnet stuff

--- pi-code/TelnetClient.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 4444                # Reserve a port for your service.
-
-s.connect((host, port))
-print s.recv(1024)
-s.close()
