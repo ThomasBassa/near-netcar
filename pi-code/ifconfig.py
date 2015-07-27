8000bb3fb383966cc7e48c112b9119af9d98f747
Alex Spradlin <spradlia@my.erau.edu>
2015-07-27 13:48:31 -0400

    Finished ip-fetching code and removed unnecessary program fragments

--- pi-code/ifconfig.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-def ifconfig():
-	os.system("ifconfig > ipdata")
-	with open ("ipdata.txt", "r") as myfile:
-		data = myfile.read().replace('/n', '')
-	print data
-ifconfig	
